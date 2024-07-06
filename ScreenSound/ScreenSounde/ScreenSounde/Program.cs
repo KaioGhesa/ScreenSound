5ae75a03ea879ff87b48a007de3265838b62c89d
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-07-06 10:10:47 -0300

    Add files via upload

---
+++ ScreenSound/ScreenSounde/ScreenSounde/Program.cs
@@ -0,0 +1,86 @@
+﻿using ScreenSound.Menus;
+using ScreenSound.Modelos;
+using ScreenSounde.Banco;
+
+
+/** A instrução using tem como objetivo principal garantir que objetos descartáveis sejam utilizados corretamente. 
+    * Quando declaramos uma variável local como using, ela é descartada no final do escopo em que ela foi declarada
+    */
+try
+{
+    var artistaDAL = new ArtistaDAL();
+    artistaDAL.Adicionar(new Artista("Foo Fighters", "Foo Fighters é uma banda americana de rock formada em 1994, em Seattle, Washington."));
+
+    var listaArtistas = artistaDAL.Listar();
+
+    foreach ( var artist in listaArtistas)
+    {
+        Console.WriteLine(artist);
+    }
+
+    /*Neste exemplo utilizado no curso, a variável connection foi declarada como using, portanto, será descartada ao finalizar a execução do try. 
+     * Com isso conseguimos aplicar uma boa prática e 
+     * gerenciar melhor os recursos que estão sendo utilizados e mantê-los somente quando estiverem sendo utilizados.*/
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
+return;
+
+Artista ira = new Artista("Ira!", "Banda Ira!");
+Artista beatles = new("The Beatles", "Banda The Beatles");
+
+Dictionary<string, Artista> artistasRegistrados = new();
+artistasRegistrados.Add(ira.Nome, ira);
+artistasRegistrados.Add(beatles.Nome, beatles);
+
+Dictionary<int, Menu> opcoes = new();
+opcoes.Add(1, new MenuRegistrarArtista());
+opcoes.Add(2, new MenuRegistrarMusica());
+opcoes.Add(3, new MenuMostrarArtistas());
+opcoes.Add(4, new MenuMostrarMusicas());
+opcoes.Add(-1, new MenuSair());
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar um artista");
+    Console.WriteLine("Digite 2 para registrar a música de um artista");
+    Console.WriteLine("Digite 3 para mostrar todos os artistas");
+    Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+    {
+        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(artistasRegistrados);
+        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida");
+    }
+}
+
+ExibirOpcoesDoMenu();
